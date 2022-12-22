4bd410f00f2e68291ad39f3eb5f88d521ba8a5e1
lucaslemo <lucaslemo852@gmail.com>
2022-12-22 09:09:19 -0300

    Fix: Titulo da pagina

--- View/inicio-html.php
+++ View/inicio-html.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title><?= $titulo; ?></title>
+        <title><?= 'Titulo' ?></title>
         <link href="/assets/css/styles.css" rel="stylesheet" type="text/css"/>
         <link href="/assets/css/datatable.css" rel="stylesheet" type="text/css"/>
     </head>
